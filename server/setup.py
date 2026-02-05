39fd2b2439ee7c66acfef5ecd9ba6640b6d10c24
RFlintstone <rubenflint@hotmail.com>
2026-02-05 22:19:54 +0100

    feat: add docker-compose configuration for client service

--- server/setup.py
+++ server/setup.py
@@ -1,29 +1,27 @@
-from distutils.core import setup
-
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 install_requires = [
-	'eventlet~=0.33.3',
-	'joycontrol @ git+https://github.com/mart1nro/joycontrol.git@721646a7ec10231490bcf788b3bdde10d8c2007f',
-	'flask-socketio~=5.6.0',
+    'eventlet~=0.33.3',
+    'joycontrol @ git+https://github.com/mart1nro/joycontrol.git@721646a7ec10231490bcf788b3bdde10d8c2007f',
+    'flask-socketio~=5.6.0',
 ]
 
 test_deps = [
-	'pytest',
+    'pytest',
 ]
 
 setup(
-	name='switch-remoteplay-server',
-	version='1.0.0',
-	packages=find_packages(),
-	url='https://github.com/juharris/switch-remoteplay',
-	license='MIT',
-	author="Justin D. Harris",
-	author_email='',
-	description="Play your Nintendo Switch remotely. Run this on a device that can connect to a Switch.",
-	install_requires=install_requires,
-	tests_require=test_deps,
-	extras_require=dict(
-			test=test_deps,
-	),
+    name='switch-remoteplay-server',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/juharris/switch-remoteplay',
+    license='MIT',
+    author="Justin D. Harris",
+    author_email='',
+    description="Play your Nintendo Switch remotely. Run this on a device that can connect to a Switch.",
+    install_requires=install_requires,
+    tests_require=test_deps,
+    extras_require=dict(
+        test=test_deps,
+    ),
 )
